02e5d903866d63dc55e8eeed5076593ac3b6c36a
kurasa1124 <kurasa1124@gmail.com>
2019-05-27 21:15:52 +0800

    fixed LightBox

--- src/app/component/light-box/light-box.component.ts
+++ src/app/component/light-box/light-box.component.ts
@@ -1,9 +1,23 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-light-box',
   templateUrl: './light-box.component.html',
-  styleUrls: ['./light-box.component.scss']
+  styleUrls: ['./light-box.component.scss'],
+  host: { "[@slideIn]": "" },
+  animations: [
+    trigger("slideIn", [
+      transition(":enter", [
+        style({ opacity: 0 }),
+        animate(".25s ease-in-out", style({ opacity: 1 }))
+      ]),
+      transition(":leave", [
+        style({ opacity: 1 }),
+        animate(".25s ease-in-out", style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class LightBoxComponent implements OnInit {
   @Output() showBox = new EventEmitter();
