cf1ca3fb32916d601ab605392194cbbe2354e99d
kurasa1124 <kurasa1124@gmail.com>
2019-06-03 05:06:25 +0800

    fix style

--- src/app/page/detail/content/content.component.ts
+++ src/app/page/detail/content/content.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as chinese from "chinesegen"
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.scss']
 })
 export class ContentComponent implements OnInit {
-
+  public h3 = chinese({ count: 10, freq: true }).text;
+  public h4 = chinese({ count: 20, freq: true }).text;
+  public p = chinese({ count: 150, freq: true }).text
   constructor() { }
 
   ngOnInit() {
