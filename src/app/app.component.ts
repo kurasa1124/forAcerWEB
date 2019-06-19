a80429be538b3407ebbe3e24a76eb6643b7be6d0
kurasa1124 <kurasa1124@gmail.com>
2019-06-20 01:52:38 +0800

    head scroll in to view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,19 @@ export class AppComponent implements OnInit {
       this.showMenu = false;
       let container = document.getElementById("container");
       container.scrollTop = 0;
+      this._scrollToActive();
     });
+  }
+
+  private _scrollToActive() {
+    setTimeout(() => {
+      let active = document.querySelector(".header li.active") as HTMLElement;
+      if (!active) return;
+      let parent = active.parentElement;
+      let scrollLeft = parent.lastChild == active ? active.offsetLeft : active.offsetLeft - active.offsetWidth;
+      let isInView = active.offsetLeft + active.clientWidth > parent.scrollLeft && active.offsetLeft < parent.scrollLeft + parent.clientWidth;
+      if (isInView) return;
+      if (active) active.parentElement.scrollTo(scrollLeft, 0);
+    }, 500);
   }
 }
